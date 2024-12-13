344c6664d5d41fa8b5e93d768f935f37cdef103c
Xiang Li <bigxiang@gmail.com>
2024-12-13 10:50:20 +0800

    Add day 8

--- day8/p2.rb
+++ day8/p2.rb
@@ -23,8 +23,8 @@ f.each_line do |line|
   original_matrix << line.strip.split('')
 end
 
-puts original_matrix.size
-puts original_matrix[0].size
+# puts original_matrix.size
+# puts original_matrix[0].size
 # puts original_matrix.map(&:join).join("\n")
 
 antennas = {}
@@ -49,25 +49,29 @@ puts antenna_pairs[0].map { |pair| pair.join(',') }.join(':')
 
 anti_nodes = Set.new
 antenna_pairs.each do |pair|
-  p1, p2 = pair
+  p1, p2 = pair.map(&:dup)
 
   puts '==='
-  puts p1.join(','), p2.join(',')
-
   di = p1[0] - p2[0]
   dj = p1[1] - p2[1]
-  p1_anti_node = [p1[0] + di, p1[1] + dj]
-  p2_anti_node = [p2[0] - di, p2[1] - dj]
-  puts p1_anti_node.join(','), p2_anti_node.join(',')
-  puts '---'
-  if p1_anti_node[0] >= 0 && p1_anti_node[0] < original_matrix.size &&
-     p1_anti_node[1] >= 0 && p1_anti_node[1] < original_matrix[0].size
-    anti_nodes.add(p1_anti_node)
+  puts "#{di}, #{dj}, #{p1}, #{p2}"
+
+  while p1[0] >= 0 && p1[0] < original_matrix.size &&
+        p1[1] >= 0 && p1[1] < original_matrix[0].size
+
+    puts p1.join(',')
+    anti_nodes.add(p1.dup)
+    p1[0] += di
+    p1[1] += dj
   end
 
-  if p2_anti_node[0] >= 0 && p2_anti_node[0] < original_matrix.size &&
-     p2_anti_node[1] >= 0 && p2_anti_node[1] < original_matrix[0].size
-    anti_nodes.add(p2_anti_node)
+  while p2[0] >= 0 && p2[0] < original_matrix.size &&
+        p2[1] >= 0 && p2[1] < original_matrix[0].size
+
+    puts p2.join(',')
+    anti_nodes.add(p2.dup)
+    p2[0] -= di
+    p2[1] -= dj
   end
 end
 
