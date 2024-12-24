8609877ba4d1429d3e5d93bdabd2485cca4271f8
Xiang Li <bigxiang@gmail.com>
2024-12-24 19:50:29 +0800

    Update day 21

--- day21/p2.rb
+++ day21/p2.rb
@@ -59,9 +59,13 @@ dir_map = {
   [-1, 0] => '^'
 }
 
-def shortest_seq(code, pad, pad_map)
-  # puts "code: #{code}"
-  result = []
+def shortest_seq(code, n, pad, pad_map, cache)
+  if cache.key?(code)
+    result = cache[code].transform_values { |count| count * n }
+    return result
+  end
+
+  result = {}
 
   start_point = pad_map['A']
   sharp_point = pad_map['#']
@@ -70,8 +74,6 @@ def shortest_seq(code, pad, pad_map)
     target = pad_map[c]
     i_distance = target[0] - start_point[0]
     j_distance = target[1] - start_point[1]
-
-    # puts "i_distance: #{i_distance}, j_distance: #{j_distance}"
 
     if start_point[0] == sharp_point[0] && target[1] == sharp_point[1]
       i_distance.abs.times do
@@ -113,97 +115,76 @@ def shortest_seq(code, pad, pad_map)
       j_distance.abs.times do
         steps << (j_distance > 0 ? '>' : '<')
       end
-    elsif j_distance > 0
-      j_distance.abs.times do
-        steps << (j_distance > 0 ? '>' : '<')
-      end
-
+    elsif i_distance < 0
       i_distance.abs.times do
         steps << (i_distance > 0 ? 'v' : '^')
+      end
+
+      j_distance.abs.times do
+        steps << (j_distance > 0 ? '>' : '<')
       end
     else
-      i_distance.abs.times do
-        steps << (i_distance > 0 ? 'v' : '^')
-      end
-
       j_distance.abs.times do
         steps << (j_distance > 0 ? '>' : '<')
       end
+
+      i_distance.abs.times do
+        steps << (i_distance > 0 ? 'v' : '^')
+      end
+      # elsif j_distance > 0
+      #   j_distance.abs.times do
+      #     steps << (j_distance > 0 ? '>' : '<')
+      #   end
+
+      #   i_distance.abs.times do
+      #     steps << (i_distance > 0 ? 'v' : '^')
+      #   end
+      # else
+      #   i_distance.abs.times do
+      #     steps << (i_distance > 0 ? 'v' : '^')
+      #   end
+
+      #   j_distance.abs.times do
+      #     steps << (j_distance > 0 ? '>' : '<')
+      #   end
     end
 
     steps << 'A'
-    result << steps
+    result[steps] = result.fetch(steps, 0) + 1
     start_point = target
   end
 
-  result.flatten
+  cache[code] = result
+  result.transform_values { |count| count * n }
 end
 
 sum = 0
+cache = {}
 codes.each do |code|
-  r_sequences = shortest_seq(code, numpad, numpad_map)
-  puts r_sequences.join
+  r_sequences = shortest_seq(code, 1, numpad, numpad_map, {})
+  puts r_sequences
 
-  # 1.times do
-  #   r_sequences = r_sequences.flat_map do |s|
-  #     shortest_seq(s, dirpad, dirpad_map, dir_map)
-  #   end
-  # end
+  25.times do
+    r_sequences = r_sequences.each_with_object({}) do |(cd, count), memo|
+      new_seq = shortest_seq(cd, count, dirpad, dirpad_map, cache)
+      memo.keys.each do |k|
+        memo[k] += new_seq.fetch(k, 0)
+      end
 
-  r_sequences = '<^^^AvvvA>^AvA'.split('')
-  # r_sequences = shortest_seq(r_sequence, dirpad, dirpad_map, dir_map)
-
-  # puts r_sequences.map { |r| r.join }.join("\n")
-
-  1.times do
-    r_sequences = shortest_seq(r_sequences, dirpad, dirpad_map)
+      new_seq.keys.each do |k|
+        memo[k] = new_seq[k] unless memo.key?(k)
+      end
+    end
   end
 
-  # r_sequences = [
-  #   '<AAAv<A>>^Av<AAA^>A<Av>A^Av<A^>A',
-  #   '<AAAv<A>>^Av<AAA^>A<Av>A^Av<A>^A',
-  #   '<AAAv<A>>^Av<AAA^>A<Av>A^A<vA^>A',
-  #   '<AAAv<A>>^Av<AAA^>A<Av>A^A<vA>^A',
-  #   '<AAAv<A>>^Av<AAA^>A<A>vA^Av<A^>A',
-  #   '<AAAv<A>>^Av<AAA^>A<A>vA^Av<A>^A',
-  #   '<AAAv<A>>^Av<AAA^>A<A>vA^A<vA^>A',
-  #   '<AAAv<A>>^Av<AAA^>A<A>vA^A<vA>^A',
-  #   '<AAAv<A>>^Av<AAA>^A<Av>A^Av<A^>A',
-  #   '<AAAv<A>>^Av<AAA>^A<Av>A^Av<A>^A',
-  #   '<AAAv<A>>^Av<AAA>^A<Av>A^A<vA^>A',
-  #   '<AAAv<A>>^Av<AAA>^A<Av>A^A<vA>^A',
-  #   '<AAAv<A>>^Av<AAA>^A<A>vA^Av<A^>A',
-  #   '<AAAv<A>>^Av<AAA>^A<A>vA^Av<A>^A',
-  #   '<AAAv<A>>^Av<AAA>^A<A>vA^A<vA^>A',
-  #   '<AAAv<A>>^Av<AAA>^A<A>vA^A<vA>^A',
-  #   '<AAAv<A>>^A<vAAA^>A<Av>A^Av<A^>A',
-  #   '<AAAv<A>>^A<vAAA^>A<Av>A^Av<A>^A',
-  #   '<AAAv<A>>^A<vAAA^>A<Av>A^A<vA^>A',
-  #   '<AAAv<A>>^A<vAAA^>A<Av>A^A<vA>^A',
-  #   '<AAAv<A>>^A<vAAA^>A<A>vA^Av<A^>A',
-  #   '<AAAv<A>>^A<vAAA^>A<A>vA^Av<A>^A',
-  #   '<AAAv<A>>^A<vAAA^>A<A>vA^A<vA^>A',
-  #   '<AAAv<A>>^A<vAAA^>A<A>vA^A<vA>^A',
-  #   '<AAAv<A>>^A<vAAA>^A<Av>A^Av<A^>A',
-  #   '<AAAv<A>>^A<vAAA>^A<Av>A^Av<A>^A',
-  #   '<AAAv<A>>^A<vAAA>^A<Av>A^A<vA^>A',
-  #   '<AAAv<A>>^A<vAAA>^A<Av>A^A<vA>^A',
-  #   '<AAAv<A>>^A<vAAA>^A<A>vA^Av<A^>A',
-  #   '<AAAv<A>>^A<vAAA>^A<A>vA^Av<A>^A',
-  #   '<AAAv<A>>^A<vAAA>^A<A>vA^A<vA^>A',
-  #   '<AAAv<A>>^A<vAAA>^A<A>vA^A<vA>^A'
-  # ]
-  # r_sequences = r_sequences.flat_map do |s|
-  #   shortest_seq(s, dirpad, dirpad_map, dir_map)
-  # end
-
-  puts r_sequences.join
-
-  min_ops = r_sequences.size
+  min_ops = 0
+  r_sequences.each do |cd, count|
+    min_ops += (cd.size * count)
+  end
   num = code[0..code.size - 2].join.to_i
-
   sum += num * min_ops
   puts "#{num} * #{min_ops} = #{num * min_ops}"
 end
 
 puts sum
+puts cache
